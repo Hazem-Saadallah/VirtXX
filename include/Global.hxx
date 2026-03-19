8c548246a59a54d849a3483351b2a4839b8eff5f
HazemA-Saadallah <hazema.saadallah@gmail.com>
2026-03-19 02:56:06 +0200

    fixed Global::SilenceStderrReporting()

--- include/Global.hxx
+++ include/Global.hxx
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <libvirt/virterror.h>
+
+void _SilentErrorHandler([[maybe_unused]] void *userData, [[maybe_unused]] virErrorPtr error);
+
 namespace Global {
   void SilenceStderrReporting();
+  
 }
--- src/Global.cxx
+++ src/Global.cxx
@@ -2,6 +2,8 @@
 #include <libvirt/virterror.h>
 #include <Global.hxx>
 
+void _SilentErrorHandler([[maybe_unused]] void *userData, [[maybe_unused]] virErrorPtr error) { }
+
 void Global::SilenceStderrReporting() {
-  virSetErrorFunc(NULL, NULL);
+  virSetErrorFunc(NULL, _SilentErrorHandler);
 }
